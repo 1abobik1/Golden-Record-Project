80ed84153cd06a2a0cb1a0715ebf7b9afd9055f3
1abobik1 <dima15042004@mail.ru>
2024-11-11 14:02:41 +0300

    add connection

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,1 +1,23 @@
-# Утилита для подключения к базе данных
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Настройки подключения
+DATABASE_URL = "postgresql://postgres:dima15042004@localhost:5432/golden_record_db"
+
+# Создание движка для подключения
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Базовый класс для моделей
+Base = declarative_base()
+
+# Создание сессии
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Функция для получения сессии
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- utils/logging_config.py
+++ utils/logging_config.py
@@ -1,2 +1,22 @@
-# в папке utils будут вспомогательные функции
-# Конфигурация для логирования к примеру для файла logging_config.py
+# файл возможно не понадобиться, если мы не будем использовать логгер
+
+import logging
+
+def setup_logging():
+    # Настройка базового конфигуратора логирования
+    logging.basicConfig(
+        level=logging.DEBUG,  # Уровень логирования (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Формат вывода
+        handlers=[
+            logging.FileHandler("app.log"),  # Логирование в файл
+            logging.StreamHandler()  # Логирование в консоль
+        ]
+    )
+
+# Пример использования
+setup_logging()
+
+logger = logging.getLogger(__name__)
+logger.debug("Это сообщение отладки.")
+logger.info("Это информационное сообщение.")
+logger.warning("Это предупреждение.")
