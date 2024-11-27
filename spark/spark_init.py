89e45fcc7406d69d1d71c02d900554b54ab45774
1abobik1 <dima15042004@mail.ru>
2024-11-27 16:30:20 +0300

    add temp files

--- spark/spark_init.py
+++ spark/spark_init.py
@@ -1,8 +1,13 @@
 from pyspark.sql import SparkSession
+import os
+
+
+os.environ['PYSPARK_PYTHON'] = r"C:\Users\dima1\PycharmProjects\golden_record_project\.venv\Scripts\python.exe"
+os.environ['PYSPARK_DRIVER_PYTHON'] = r"C:\Users\dima1\PycharmProjects\golden_record_project\.venv\Scripts\python.exe"
 
 spark = SparkSession.builder \
     .appName("APP") \
-    .config("spark.sql.shuffle.partitions", "100") \
+    .config("spark.sql.shuffle.partitions", "200") \
     .config("spark.executor.memory", "8g") \
     .config("spark.executor.cores", "2") \
     .config("spark.driver.memory", "8g") \
